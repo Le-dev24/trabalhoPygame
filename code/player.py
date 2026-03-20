cb42776df6bb9b19115f292a20e5735fe18e3f29
Letícia Cardoso <leticia24.c.ramos@gmail.com>
2026-03-20 17:00:53 -0300

    classe player implementada com movimentação e renderização

---
+++ code/player.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        # tamanho
+        self.largura = 50
+        self.altura = 50
+
+        # velocidade
+        self.vel = 5
+
+        # retângulo usado para a colisão
+        self.rect = pygame.Rect(self.x, self.y, self.largura, self.altura)
+
+    def mover(self, teclas):
+        if teclas[pygame.K_LEFT]:
+            self.x -= self.vel
+        if teclas[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        # atualiza a posição do retângulo
+        self.rect.topleft = (self.x, self.y)
+
+    def desenhar(self, tela):
+        pygame.draw.rect(tela(tela, (0, 0, 255), self.rect)
+
